21c1c20e17e287171329919e14aaa32e81ade25d
Michel Barret <michel.barret@gmail.com>
2015-08-21 11:28:16 +0200

    Add log

--- src/main/java/net/bons/commptes/integration/Configuration.java
+++ src/main/java/net/bons/commptes/integration/Configuration.java
@@ -1,11 +1,17 @@
 package net.bons.commptes.integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Configuration {
+  private static final Logger LOG = LoggerFactory.getLogger(Configuration.class);
   public int getPort() {
     String port = System.getenv("PORT");
     if (port == null) {
       port = System.getProperty("PORT", "5000");
     }
+
+    LOG.info("Listen on {}", port);
     return Integer.valueOf(port);
   }
 }
